15716d0bb0b087777d61d6ccf21a5f005f2cf0ac
Louis <louis.zerri@hotmail.com>
2018-12-10 11:44:27 +0100

    Creation des champs text et bouton

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -4,21 +4,44 @@ import './App.css';
 
 const TodoItem = (props) => <li>{props.description}</li>
 
-let todoList = ["Aller à la plage",
-				"Faire des courses",
-				"Ranger les courses",
-				"Faire la cuisine",
-				"Ranger la cuisine"]
-
 class TodoApp extends Component {
+
+	constructor(){
+		super();
+		this.state = {
+			currentTodo : "",
+			list : ["Aller à la plage", "Faire des courses", "Ranger les courses", "Faire la cuisine", "Ranger la cuisine"]
+		}
+
+		this.changeCurrentTodo = this.changeCurrentTodo.bind(this)
+		this.addTodo = this.addTodo.bind(this)
+	}
+
+	addTodo()
+	{
+		//Ajouter au tableau this.state.list, le contenu du champ text
+		this.state.list.push(this.state.currentTodo)
+		this.render()
+
+	}
+
+	changeCurrentTodo(event)
+	{
+		console.log(`Contenu du champ text : ${event.target.value}`)
+		this.setState({
+			currentTodo : event.target.value
+		})
+	}
 	
 	render() {
     	return (
-      		<div>
-      			<ul>
-              		{todoList.map(element => <TodoItem description={element}/>)}
-      			</ul>
-      		</div>
+      	<div>
+				<input onChange={this.changeCurrentTodo} type="text" name="todoText" value={this.state.currentTodo}/>
+				<input onClick={this.addTodo} type="submit" value="Add"/>
+      		<ul>
+            	{this.state.list.map(element => <TodoItem description={element}/>)}
+      		</ul>
+      	</div>
     	);
   	}
 }
